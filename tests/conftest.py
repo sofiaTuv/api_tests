c9104f2a2cc3834f5dc4cb55fdb4752e06e0b8b1
Sofia Tuvykina <hdfaa.56@gmail.com>
2023-12-12 14:02:05 +0300

    added browser setup and url demoshop

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,16 @@
 import pytest
+from selene import browser
 
 
 @pytest.fixture
 def url():
     return 'https://reqres.in/api'
+
+
+@pytest.fixture(scope="function")
+def browser_setup():
+    browser.config.base_url = 'https://demowebshop.tricentis.com'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    yield
+    browser.quit()
